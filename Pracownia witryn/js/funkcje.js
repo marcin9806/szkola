95c15526a4c8e0a6f69f6fe764f971e205614e9d
marcin9806 <marcin.dom26@hotmail.com>
2017-09-13 10:39:21 +0200

    funkcje JS2

--- Pracownia witryn/js/funkcje.js
+++ Pracownia witryn/js/funkcje.js
@@ -1,22 +1,18 @@
-function witaj(imie)
-{
-    document.write("Witaj");
-}
 function poleProstokata(a,b)
 {
     var pole=a*b;
     return pole;
 }
-var elWysokosc=document.getElementById("wysokosc").value;
-var elSzerokosc=document.getElementById("szerokosc").value;
+
 var elPrzycisk=document.getElementById("przycisk");
 var elWyswietl=document.getElementById("wyswietl");
 
 function wyswietlPole()
 {
+    
+    var elWysokosc=document.getElementById("wysokosc").value;
+    var elSzerokosc=document.getElementById("szerokosc").value;
+    console.log(elSzerokosc);
+    console.log(elSzerokosc);
     elWyswietl.innerHTML = poleProstokata(elWysokosc,elSzerokosc);
-    console.log(elSzerokosc);
-    console.log(elSzerokosc);
 }
-
-witaj(name);
