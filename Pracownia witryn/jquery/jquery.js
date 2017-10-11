2d5965d482e1e649671e38f1d109a52c16246e94
marcin9806 <marcin.dom26@hotmail.com>
2017-10-11 09:05:20 +0200

    jquery

---
+++ Pracownia witryn/jquery/jquery.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+    $("#tekst").click(function(){
+        $("#lorem").toggle(10);
+        $("div").css("background","yellow");
+        
+        $(".tekst").css("background","teal");
+        $("#id1").css("background","red");
+        $("#id1").css({"background":"white","color":"red"});
+        $("p").css("font-family","arial","verdana");
+        $("p").css({"color":"red","font-size":"120%"});
+    })
+    $("#zmienKolor").click(function(){
+        $("#zmienKolor").css("color","red");
+    });
+});
+
